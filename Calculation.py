9c8f05c708ef64d8eea2461fc809207a26872d09
lawinse <fjfzbwj@163.com>
2016-12-05 13:23:11 +0800

    Finish ransac

--- Calculation.py
+++ Calculation.py
@@ -30,8 +30,10 @@ class Calculation:
 		# self.__ransac.restype = [ctypes.POINTER(ctypes.c_double)]
 
 	def process(self,fname):
+		print ">>>>> begin process",fname
 		bscores = self.detect_boundary(fname, r_neigh=1.2);
-		# self.ransac(bscores);
+		target_hyp,max_inlier = self.ransac(bscores);
+		print ">>>>> final sun direction:", target_hyp," with %d inliers" % max_inlier
 
 	@classmethod
 	def __standard_normals(cls,v):
@@ -56,7 +58,8 @@ class Calculation:
 	def Get_angel_degree(cls,v1,v2):
 		x1,y1,z1 = cls.__standard_normals(v1);
 		x2,y2,z2 = cls.__standard_normals(v2);
-		return math.acos(abs(x1*x2+y1*y2+z1*z2)/(math.sqrt(x1**2 + y1**2 + z1**2)*math.sqrt(x2**2 + y2**2 + z2**2)))*180/math.pi;
+		tmp = min(1.0,abs(x1*x2+y1*y2+z1*z2)/(math.sqrt(x1**2 + y1**2 + z1**2)*math.sqrt(x2**2 + y2**2 + z2**2)))
+		return math.acos(tmp)*180/math.pi;
 
 	def __report_boundary_status(self, bscores):
 		angel_li = [0 for i in range(7)]
@@ -85,7 +88,6 @@ class Calculation:
 	def detect_boundary(self, fname, r_neigh, b0=0.2, xita_crease=45):
 		BScores = {} # {(x,y,z,nx,ny,nz):Bpos}  type(Bpos):numpy.array(shape=(3,1))
 		if os.path.exists(Calculation.GetBoundaryDataPath(fname,r_neigh,b0,xita_crease)):
-			pass
 			BScores = joblib.load(Calculation.GetBoundaryDataPath(fname,r_neigh,b0,xita_crease))
 		else:
 			self.__est.clear_label();
@@ -132,7 +134,7 @@ class Calculation:
 			joblib.dump(BScores,Calculation.GetBoundaryDataPath(fname,r_neigh,b0,xita_crease),compress=3);
 
 		print "#Valid boundary pixel:",len(BScores)
-		self.__report_boundary_status(BScores)
+		# self.__report_boundary_status(BScores)
 		# cnt = 0;
 		# for item in BScores:
 		# 	if BScores[item][2] > 0: cnt += 1;
@@ -153,18 +155,19 @@ class Calculation:
 		ret = []
 		# 30,45,60,80
 		alpha_xita = {
-			PI/6:(10,PI/20),
-			PI/4:(8,0),
-			PI/3:(6,PI/12),
-			PI*0.45:(4,0),
+			PI/6:(200,0),
+			PI/4:(200,0),
+			PI/3:(200,0),
+			PI*0.48:(200,0),
 			PI/2:(1,0),
 		}
 		for a in alpha_xita.keys():
 			diff,t = alpha_xita[a];
 			while (t<2*PI):
 				ret.append((math.cos(a)*math.cos(t),math.cos(a)*math.sin(t), math.sin(a)));
-				t += 2*PI/diff;
-		kdtree = spatial.KDTree([tuple(item) for item in ret]);
+				t += 2.0*PI/diff;
+		kdtree = spatial.KDTree(ret);
+		# print ret
 		return ret,kdtree;
 
 	def __generate_hyp(self,inliers):
@@ -174,55 +177,124 @@ class Calculation:
 		return self.__standard_normals((-self.__clf.coef_[0],-self.__clf.coef_[1],1))
 
 
-	def ransac(self, bscores):
-		# def dot_prod(x,y):
-		# 	return np.dot(x,y);
-		# def cross_prod(x,y):
-		# 	return np.cross(x,y)
+	def ransac(self, bscores, angel_e=10, stop_e=0.1, inner_max_iter=100, outter_max_iter=30):
+		import random
+		## Policy
+		USE_BINS = False;
+		FILTER_BOUNDERY_VEC = False;
+		print "[USE_BINS]:",USE_BINS
+		print "[FILTER_BOUNDERY_VEC]:",FILTER_BOUNDERY_VEC
 
-		bscores = self.__filter_boudary_by_angel(bscores, 40);
+
+		if FILTER_BOUNDERY_VEC: bscores = self.__filter_boudary_by_angel(bscores, 30);
 		normals = [0 for i in xrange(len(bscores))];
+		pos = [0 for i in xrange(len(bscores))];
 		bvs = [0 for i in xrange(len(bscores))];
 		cnt = 0;
 		for bnode in bscores.keys():
 			normals[cnt] = np.array(self.__standard_normals(PlyUtils.Get_normals(bnode)));
 			bvs[cnt] = np.array(self.__standard_normals(bscores[bnode]));
+			pos[cnt] = np.array(PlyUtils.Get_pos(bnode))
 			cnt += 1;
-		# normals = np.array(normals);
-		# bvs = np.array(bvs);
 
-		## Step-1: Initially, find two most likely vectors as the initial vectors
-		best_two = [(-1,0),(-1,0)];
-		for idx in xrange(len(normals)):
-			angel = self.Get_angel_degree(normals[idx],bvc[idx]);
-			if (angel > best_two[0][1]):
-				best_two[1] = best_two[0];
-				best_two[0] = (idx,angel);
-			elif (angel > best_two[1][1]):
-				best_two[1] = (idx,angel);
-		cur_hyp = self.__standard_normals(np.cross(normals[best_two[0][0]], normals[best_two[1][0]]));
-		if cur_hyp[2] < 0: cur_hyp = (-cur_hyp[0],-cur_hyp[1],-cur_hyp[2])   # recorrect the sun direction
+		
+		out_iter = 0;
+		opt_hyp = None;
+		opt_max_inner_cnt = -1;
+		while out_iter<outter_max_iter:
+			print "\n##### OUTTER ITER %d ######\n" % out_iter
+			out_iter += 1;
+			## Step-1: Initially, randomly find two vectors as the initial vectors
+			# best_two = [(-1,0,[0,0,0]),(-1,0,[0,0,0])];
+			# for idx in xrange(len(normals)):
+			# 	# print best_two
+				
+			# 	angel = self.Get_angel_degree(normals[idx],bvs[idx]);
+			# 	if (angel > best_two[0][1] and list(normals[idx]) != best_two[0][2]):
+			# 		best_two[1] = best_two[0];
+			# 		best_two[0] = (idx,angel,list(normals[idx]));
+			# 	elif (angel > best_two[1][1] and list(normals[idx]) != best_two[1][2] and list(normals[idx]) != best_two[0][2]):
+			# 		best_two[1] = (idx,angel,list(normals[idx]));
+			# print pos[best_two[0][0]],pos[best_two[1][0]]
 
-		## Step-2: Get pre-computed bin-vectors and quantize the surface normals
-		bin_vectors, bv_kdtree = self.get_bin_vectors();
-		bv_bins = [[] for i in range(len(bin_vectors))];
-		bv_bins_avers = [0 for i in range(len(bin_vectors))]
-		bv_map = {};
+			# init_one, init_two = normals[best_two[0][0]], normals[best_two[1][0]]
+			init_one, init_two = random.sample(set([str(list(item)) for item in normals]),2);
+			init_one = eval(init_one);
+			init_two = eval(init_two)
 
-		for idx in xrange(len(normals)):
-			nearest_idx = bv_kdtree.query(normals[idx]);
-			bv_bins[nearest_idx].append(idx);
-			bv_map[idx] = nearest_idx;
-		for i,Bin in enumerate(bv_bins):
-			if len(Bin) == 0:
-				bv_bins_avers[i] = None;
-			else:
-				bv_bins_avers[i] = self.__standard_normals(self.__get_centroid(Bin));
+			cur_hyp = self.__standard_normals(np.cross(init_one, init_two));
 
-		## Step-3 loop
-		# while 1:
+			if cur_hyp[2] < 0: cur_hyp = (-cur_hyp[0],-cur_hyp[1],-cur_hyp[2])   # recorrect the sun direction
+			pre_hyp = None;
 
+			## Step-2: Get pre-computed bin-vectors and quantize the surface normals
+			bin_vectors, bv_kdtree = self.get_bin_vectors();
+			bv_map = {};
+			for idx in xrange(len(normals)):
+				dis,nearest_idx = bv_kdtree.query(normals[idx]);
+				# print normals[idx]
+				bv_map[idx] = nearest_idx;
 			
+
+			## Step-3 loop
+			inner_iter = 0;
+			while inner_iter < inner_max_iter:
+				inner_iter += 1;
+				### Step-3.1 filter out specific boundary points and throw into bins
+				select_bins = set();
+				# helper = set()
+				bv_bins = [[] for i in range(len(bin_vectors))];
+				bv_bins_avers = [0 for i in range(len(bin_vectors))]
+				for i in xrange(len(normals)):
+					angel = self.Get_angel_degree(normals[i],cur_hyp);
+					if angel<90+angel_e and angel>90-angel_e and np.dot(np.asarray(cur_hyp),bvs[i])>0:
+						bv_bins[bv_map[i]].append(normals[i]);
+						# helper.add(str(normals[i]));
+						select_bins.add(bv_map[i]);
+				# print ">>>>>> [helper] distinct normal inliers", helper
+				
+				### Step-3.2 calc inliers
+				if USE_BINS:
+					for i,Bin in enumerate(bv_bins):
+						if len(Bin) == 0:
+							bv_bins_avers[i] = None;
+						else:
+							bv_bins_avers[i] = self.__standard_normals(self.__get_centroid(Bin));
+
+
+					inliers = []
+					for b in iter(select_bins):
+						if bv_bins_avers[b] != None:
+							inliers.append(bv_bins_avers[b]);
+					print '>>>>> inliers cnt', sum([len(item) for item in bv_bins])
+					print '>>>>> inliers bins cnt', len(inliers)
+					inliers_cnt = sum([len(item) for item in bv_bins])
+				else:
+					inliers = []
+					for b in iter(bv_bins):
+						for n in b:
+							inliers.append(n);
+					print '>>>>> inliers cnt', len(inliers)
+					inliers_cnt = len(inliers)
+
+				if len(inliers) == 0: break;
+
+				### Step-3.3 update hypothesis
+				pre_hyp = cur_hyp;
+				cur_hyp = self.__generate_hyp(inliers);
+				inner_max_inliers_cnt = inliers_cnt
+				print ">>>>>> inner_max_inliers_cnt",inner_max_inliers_cnt;
+
+				print ">>>>> update hyp", pre_hyp,'===>>',cur_hyp
+				# print select_bins
+				if (self.Get_angel_degree(pre_hyp,cur_hyp)<stop_e):
+					print self.Get_angel_degree(pre_hyp,cur_hyp)
+					break;
+
+			if inner_max_inliers_cnt > opt_max_inner_cnt:
+				opt_max_inner_cnt = inner_max_inliers_cnt;
+				opt_hyp = cur_hyp
+		return opt_hyp,opt_max_inner_cnt
 
 
 
@@ -239,8 +311,7 @@ class Calculation:
 
 if __name__ == '__main__':
 	calc = Calculation();
-	# print calc.get_bin_vectors();
-	calc.process(fname = "meas-00043-00000.png");
+	calc.process(fname = "meas-00010-00000.png");
 
 
 
